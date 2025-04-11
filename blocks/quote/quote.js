ed8071d8ad241f302b0da01eb0c8cfb543cbc9bd
Narendiran <narendiran100@gmail.com>
2025-04-11 17:26:15 +0530

    add suffix in blockQuote

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,17 @@
-export default function decorate(block) {
+import { fetchPlaceholders } from "../../scripts/aem.js";
+export default async function decorate(block) {
   const [quoteWrapper] = block.children;
 
   const blockquote = document.createElement("blockquote");
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
+
+  // fetch placeholders from the 'en' folder
+  const placeholders = await fetchPlaceholders();
+  const { QuoteText } = placeholders;
+  //block.appendChild(QuoteText);
+  const suffix = document.createElement("span");
+  suffix.classList.add("quote-suffix");
+  suffix.textContent = QuoteText.textContent.trim();
+  block.appendChild(suffix);
 }
